8b13beb4fa51e9c7b3f57c1b805fa2bfc6981220
surojrana69 <surojrana2014@gmail.com>
2024-07-04 20:31:35 +0545

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,14 @@
+s = int(input("Enter the first number:"))
+r = int(input("Enter the second number:"))
+ans1 = s+r
+print("Addition of",s,"and",r,"=", ans1)
+ans2 = s-r
+print("Subtraction of",s,"and",r,"=", ans2)
+ans3 = s*r
+print("Multiplication of",s,"and",r,"=", ans3)
+ans4 = s/r
+print("Division of",s,"and",r,"=", ans4)
+ans5 = s%r
+print("Modulus of",s,"and",r,"=", ans5)
+ans6 = s//r
+print("Floor Division of",s,"and",r,"=", ans6)
